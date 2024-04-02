643d023dcba6797be8aef61c218186c8fb525584
Artur Sharapov <105589311+qaip@users.noreply.github.com>
2024-04-02 13:56:45 +0300

    refactor: rm temp files for testing

--- src/api/types.ts
+++
@@ -1,5 +0,0 @@
-export interface App {
-  Album: {
-    name: string
-  }
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
-import './api/index.ts'
 
 export const mount = (id: string) => render(<App />, document.getElementById(id)!)
