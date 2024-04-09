cd5ed14c5eb0eaebb487364f04aecaa9c616d2a9
Artur Sharapov <105589311+qaip@users.noreply.github.com>
2024-04-09 14:14:12 +0300

    feat: store elements workspace-related data as relation attributes

--- src/store/reset.ts
+++ src/store/reset.ts
@@ -1,25 +1,82 @@
 import { EdgeType, NodeType } from '@ennealand/enigraph'
-import { AppWorkspace, Edge, Vertex } from './core.ts'
+import { AppWorkspace, Edge, ElementVertex, Vertex } from './core.ts'
 import { ScType } from '@ennealand/enneract'
 
 /**
  * Reset workspace sc-memory state to default mock state.
  */
 export const resetWorkspace = async () => {
+  console.groupCollapsed('Reset')
+  // const slice = (await AppWorkspace`example`.ref.one)!
+  // console.log(await slice.elementEdge.ref.delete)
+  // console.log(await slice.elementVertex.ref.delete)
+  // console.log(await slice.elementGroup.ref.delete)
+  // const vertices = await new Vertex([
+  //   { name: 'A', customType: NodeType.ConstClass, x: -39, y: -13 },
+  //   { name: 'B', customType: NodeType.ConstClass, x: 131, y: -14 },
+  //   { name: 'C', customType: NodeType.ConstClass, x: -86, y: -157 },
+  // ]).create
+  // console.log(await slice.elementVertex.link(vertices))
+  // console.log(
+  //   await slice.elementEdge.link(
+  //     new Edge({ sc: ScType.EdgeAccessVarPosPerm, customType: EdgeType.EdgeConst, from: vertices[0], to: vertices[1] }),
+  //     new Edge({ sc: ScType.EdgeAccessVarPosPerm, customType: EdgeType.EdgeConst, from: vertices[0], to: vertices[2] })
+  //   )
+  // )
   const slice = (await AppWorkspace`example`.ref.one)!
   console.log(await slice.elementEdge.ref.delete)
   console.log(await slice.elementVertex.ref.delete)
   console.log(await slice.elementGroup.ref.delete)
-  const vertices = await new Vertex([
-    { name: 'A', customType: NodeType.Const, x: -39, y: -13 },
-    { name: 'B', customType: NodeType.Const, x: 131, y: -14 },
-    { name: 'C', customType: NodeType.Const, x: -86, y: -157 },
-  ]).create
-  console.log(await slice.elementVertex.link(vertices))
-  console.log(
-    await slice.elementEdge.link(
-      new Edge({ sc: ScType.EdgeAccessVarPosPerm, customType: EdgeType.EdgeConst, from: vertices[0], to: vertices[1] }),
-      new Edge({ sc: ScType.EdgeAccessVarPosPerm, customType: EdgeType.EdgeConst, from: vertices[0], to: vertices[2] })
-    )
-  )
+
+  const vertex1 = await new Vertex({
+    name: 'A',
+    customType: NodeType.ConstClass,
+    is: { elementVertex: { value: slice, relation: { x: -39, y: -13 } } },
+  }).create
+
+  const vertex2 = await new Vertex({
+    name: 'B',
+    customType: NodeType.ConstClass,
+    is: { elementVertex: { value: slice, relation: { x: 131, y: -14 } } },
+  }).create
+
+  const vertex3 = await new Vertex({
+    name: 'C',
+    customType: NodeType.ConstClass,
+    is: { elementVertex: { value: slice, relation: { x: -86, y: -157 } } },
+  }).create
+
+  console.log(await vertex2.is.elementVertex.link(slice, { x: 10, y: 10 }))
+  console.log(await slice.elementVertex.relation.ref.addr.many)
+
+  console.log(vertex1.ref.addr, vertex2.ref.addr, vertex3.ref.addr)
+
+  const edge1 = await new Edge({
+    sc: ScType.EdgeAccessVarPosPerm,
+    customType: EdgeType.EdgeConst,
+    from: vertex1,
+    to: vertex2,
+    is: {
+      elementEdge: {
+        value: slice,
+        relation: { source: ElementVertex`${vertex1.relations[0]}`, target: ElementVertex`${vertex2.relations[0]}` },
+      },
+    },
+  }).create
+
+  const edge2 = await new Edge({
+    sc: ScType.EdgeAccessVarPosPerm,
+    customType: EdgeType.EdgeConst,
+    from: vertex1,
+    to: vertex3,
+    is: {
+      elementEdge: {
+        value: slice,
+        relation: { source: ElementVertex`${vertex1.relations[0]}`, target: ElementVertex`${vertex3.relations[0]}` },
+      },
+    },
+  }).create
+
+  console.log(edge1.ref.addr, edge2.ref.addr)
+  console.groupEnd()
 }
--- src/store/slices/workspace.ts
+++ src/store/slices/workspace.ts
@@ -1,10 +1,16 @@
 import { GraphEdge, GraphGroup, GraphNode } from '@ennealand/enigraph'
 import { ScType, deepSignal } from '@ennealand/enneract'
-import { computed } from '@preact/signals'
-import { AppWorkspace, Edge, Group, Vertex } from '../core.ts'
+import { computed, effect } from '@preact/signals'
+import { AppWorkspace, Edge, ElementVertex, Group, Vertex } from '../core.ts'
 import { $defined } from '../store.ts'
 
 const slice = (await AppWorkspace`example`.ref.one)!
+
+type GraphNodeExtended = GraphNode & { addr: number }
+type GraphEdgeExtended = GraphEdge & {
+  source: GraphNodeExtended
+  target: GraphNodeExtended
+}
 
 // Reset the workspace state:
 // await import('../reset.ts').then(mod => mod.resetWorkspace())
@@ -12,16 +18,26 @@ const slice = (await AppWorkspace`example`.ref.one)!
 /**
  * Array of verices data in the workspace
  */
-const vertices = await slice.elementVertex.get({ x: true, y: true, customType: 'type', ref: { addr: 'id' }, name: 'label' }).reactive
+const vertices = await slice.elementVertex.get({
+  relation: { x: 'x', y: 'y', ref: { addr: 'id' } },
+  ref: { addr: 'addr' },
+  customType: 'type',
+  name: 'label',
+}).reactive
+
+effect(async () => {
+  console.log(vertices.map(v => ({ id: v.id, addr: v.addr })))
+})
 
 /**
  * Array of edges data in the workspace
  */
 const edgeIds = await slice.elementEdge.get({
+  relation: { source: { ref: { addr: 'source' } }, target: { ref: { addr: 'target' } }, ref: { addr: 'id' } },
   from: { ref: { addr: 'from' } },
   to: { ref: { addr: 'to' } },
   customType: 'type',
-  ref: { addr: 'id' },
+  ref: { addr: 'addr' },
 }).reactive
 
 /**
@@ -31,10 +47,11 @@ const edges = computed(
   () => (
     console.warn('updated'),
     edgeIds
-      .map<GraphEdge | undefined>(edge => {
-        const source = vertices.find(v => v.id === edge.from)
-        const target = vertices.find(v => v.id === edge.to)
+      .map<GraphEdgeExtended | undefined>(edge => {
+        const source = vertices.find(v => v.id === edge.source)
+        const target = vertices.find(v => v.id === edge.target)
         if (!source || !target) {
+          console.log(edge, vertices)
           console.error('source or target is missing')
           return
         }
@@ -64,7 +81,7 @@ const groups = computed(() => {
  * Helper map object to associate newly created temporal node objects with the promises of their new ids.
  * This is primarly used when creating both new nodes and edge to wait for node ids to be resolved first.
  */
-const newNodeIds = new WeakMap<GraphNode, Promise<number>>()
+const newNodeIds = new WeakMap<GraphNode, { addr: Promise<number>; id: Promise<number> }>()
 
 /**
  * Create a new graph node in sc-memory associated with the current workspace.\
@@ -74,10 +91,15 @@ const newNodeIds = new WeakMap<GraphNode, Promise<number>>()
  * @param node GraphNode object with all information about new node
  */
 const addNode = async (node: GraphNode) => {
-  const newVertex = new Vertex({ x: node.x, y: node.y, customType: node.type, name: 'i', is: { elementVertex: slice } }).create
-  const id = newVertex.then(v => v.ref.addr)
-  newNodeIds.set(node, id)
-  vertices.push({ label: 'i', type: node.type, x: node.x, y: node.y, id: await id })
+  const newVertex = new Vertex({
+    customType: node.type,
+    name: 'i',
+    is: { elementVertex: { value: slice, relation: { x: node.x, y: node.y } } },
+  }).create
+  const id = newVertex.then(v => v.relations[0])
+  const addr = newVertex.then(v => v.ref.addr)
+  newNodeIds.set(node, { id, addr })
+  vertices.push({ label: 'i', type: node.type, x: node.x, y: node.y, id: await id, addr: await addr })
 }
 
 /**
@@ -86,17 +108,20 @@ const addNode = async (node: GraphNode) => {
  * with its new sc-addr used as id to eliminate delay of receiving an update from sc-memory.
  * @param node GraphEdge object with all information about new edge
  */
-const addEdge = async (edge: GraphEdge) => {
-  const from = typeof edge.source.id === 'number' ? edge.source.id : await newNodeIds.get(edge.source)!
-  const to = typeof edge.target.id === 'number' ? edge.target.id : await newNodeIds.get(edge.target)!
+const addEdge = async (edge: GraphEdgeExtended) => {
+  const from = edge.source.addr || (await newNodeIds.get(edge.source)!.addr)
+  const to = edge.target.addr || (await newNodeIds.get(edge.target)!.addr)
+  const source = edge.source.id || (await newNodeIds.get(edge.source)!.id)
+  const target = edge.target.id || (await newNodeIds.get(edge.target)!.id)
+  console.log(from, to, source, target)
   const newEdge = await new Edge({
     from: Vertex`${from}`,
     to: Vertex`${to}`,
     sc: ScType.EdgeDCommonVar,
     customType: edge.type,
-    is: { elementEdge: slice },
+    is: { elementEdge: { value: slice, relation: { source: ElementVertex`${source}`, target: ElementVertex`${target}` } } },
   }).create
-  edgeIds.push({ from, to, type: edge.type, id: newEdge.ref.addr })
+  edgeIds.push({ from, to, type: edge.type, id: newEdge.ref.addr, addr: newEdge.relations[0], source, target })
 }
 
 /**
@@ -117,9 +142,10 @@ const addGroup = async (group: GraphGroup) => {
  * @param node GraphNode object the label belongs to
  * @param label The new label value
  */
-const changeNodeLabel = async (node: GraphNode, label: string) => {
+const changeNodeLabel = async (node: GraphNodeExtended, label: string) => {
   node.label = label
-  await Vertex`${node.id}`.name.update(label)
+  await Vertex`${node.addr}`.name.update(label)
+}
 
 /**
  * Change the node position to a new one in both sc-memory associated with the current workspace
@@ -131,6 +157,7 @@ const changeNodeLabel = async (node: GraphNode, label: string) => {
 const changeNodePosition = async (node: GraphNode, x: number, y: number) => {
   node.x = x
   node.y = y
+  console.log('fone')
   await ElementVertex`${node.id}`.x.write(x)
   await ElementVertex`${node.id}`.y.write(y)
 }
