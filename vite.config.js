a236798073b69733d11ae2a46e0e56552e6c1633
Artur Sharapov <105589311+qaip@users.noreply.github.com>
2024-04-16 01:08:07 +0300

    build: exclude type definitions from build

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import preact from '@preact/preset-vite'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import dts from 'vite-plugin-dts'
 import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), preact(), cssInjectedByJsPlugin(), dts({ rollupTypes: true })],
+  plugins: [million.vite({ auto: true }), preact(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       fileName: 'index',
